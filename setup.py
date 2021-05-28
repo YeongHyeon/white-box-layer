3ead36d517db55e787d1209c6d5a7e940a3c39d9
박영현(Yeonghyeon Park)/IoT솔루션사업그룹/SKP <young200405@gmail.com>
2021-05-28 14:54:22 +0900

    20210528

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'tensorlow',
+    version             = '0.1.1',
+    description         = 'low level tensorflow custom layers',
+    author              = 'YeongHyeon Park',
+    author_email        = 'young200405@gmail.com',
+    url                 = 'https://github.com/YeongHyeon/TensorLow',
+    download_url        = 'https://github.com/YeongHyeon/TensorLow/archive/refs/heads/master.zip',
+    install_requires    = [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['tensorlow'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
