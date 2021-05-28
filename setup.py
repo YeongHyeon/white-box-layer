59c829590c9cdd530c72297745a6670dcd5ae89e
박영현(Yeonghyeon Park)/IoT솔루션사업그룹/SKP <young200405@gmail.com>
2021-05-28 14:56:34 +0900

    20210528

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name                = 'tensorlow',
+    name                = 'white-box-layer',
     version             = '0.1.1',
     description         = 'low level tensorflow custom layers',
     author              = 'YeongHyeon Park',
     author_email        = 'young200405@gmail.com',
-    url                 = 'https://github.com/YeongHyeon/TensorLow',
-    download_url        = 'https://github.com/YeongHyeon/TensorLow/archive/refs/heads/master.zip',
+    url                 = 'https://github.com/YeongHyeon/white-box-layer',
+    download_url        = 'https://github.com/YeongHyeon/white-box-layer/archive/refs/heads/master.zip',
     install_requires    = [],
     packages            = find_packages(exclude = []),
-    keywords            = ['tensorlow'],
+    keywords            = ['white-box-layer'],
     python_requires     = '>=3',
     package_data        = {},
     zip_safe            = False,
