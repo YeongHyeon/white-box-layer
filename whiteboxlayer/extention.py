6b1f101e53bd305c5aa3e5f89899b69f70f04968
YeongHyeon Park <young200405@gmail.com>
2021-05-30 19:56:36 +0900

    update

---
+++ whiteboxlayer/extention.py
@@ -0,0 +1,164 @@
+import numpy as np
+import tensorflow as tf
+
+""" -*-*-*-*-*- Extended Functions -*-*-*-*-*- """
+def trim_odd(layer, x):
+
+    b, t, c = list(x.get_shape())
+    if(int(t) % 2 == 1):
+        return tf.slice(x, [0, 0, 0], [-1, int(t)-1, -1])
+    else:
+        return x
+
+def trim_shape(layer, x, shape):
+
+    return tf.slice(x, [0, 0, 0], shape)
+
+def sub_pixel1d(layer, x, ratio, verbose=True):
+
+    y1 = tf.transpose(a=x, perm=[2, 1, 0]) # (r, w, b)
+    y2 = tf.batch_to_space(y1, [ratio], [[0, 0]]) # (1, r*w, b)
+    y3 = tf.transpose(a=y2, perm=[2, 1, 0])
+
+    if(verbose): print("SubPixel", x.shape, "->", y3.shape)
+    return y3
+
+def graph_conv(layer, x, a, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    c_in, c_out = x.get_shape().as_list()[-1], int(c_out)
+
+    w = layer.get_variable(shape=[c_in, c_out], \
+        trainable=True, name='%s_w' %(name))
+    b = layer.get_variable(shape=[c_out], \
+        trainable=True, name='%s_b' %(name))
+
+    wx = tf.linalg.matmul(x, w, name='%s_mul' %(name))
+    y_feat = tf.math.add(wx, b, name='%s_add' %(name))
+    y = tf.linalg.matmul(a, y_feat)
+
+    if(verbose): print("G-Conv (%s)" %(name), x.shape, "->", y.shape)
+
+    if(batch_norm): y = layer.batch_normalization(x=y, \
+        trainable=True, name='%s_bn' %(name), verbose=verbose)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def graph_attention(layer, x, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    wx = layer.fully_connected(x=x, c_out=c_out, \
+        batch_norm=False, activation=None, name=name, verbose=False)
+
+    y = tf.math.reduce_sum(wx, axis=-1)
+
+    if(verbose): print("Readout (%s)" %(name), x.shape, "->", y.shape)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def read_out(layer, x, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    wx = layer.fully_connected(x=x, c_out=c_out, \
+        batch_norm=False, activation=None, name=name, verbose=False)
+
+    y = tf.math.reduce_sum(wx, axis=-1)
+
+    if(verbose): print("Readout (%s)" %(name), x.shape, "->", y.shape)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def pipgcn_node_average(layer, node, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
+
+    w_node_c = layer.get_variable(shape=[node_in, c_out], \
+        trainable=True, name='%s_w_node_c' %(name))
+    b = layer.get_variable(shape=[c_out], \
+        trainable=True, name='%s_b' %(name))
+
+    """ -=-=-= Term 1: node aggregation =-=-=- """
+    term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
+
+    y = term1 + b
+    if(verbose): print("N-Avg (%s)" %(name), node.shape, "->", y.shape)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def pipgcn_node_edge_average(layer, node, edge, hood, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
+    edge_in = edge.get_shape().as_list()[-1]
+    hood = tf.squeeze(hood, axis=2)
+    hood_in = tf.expand_dims(tf.math.count_nonzero(hood + 1, axis=1, dtype=tf.float32), -1)
+
+    w_node_c = layer.get_variable(shape=[node_in, c_out], \
+        trainable=True, name='%s_w_node_c' %(name))
+    w_node_n = layer.get_variable(shape=[node_in, c_out], \
+        trainable=True, name='%s_w_node_n' %(name))
+    w_edge = layer.get_variable(shape=[edge_in, c_out], \
+        trainable=True, name='%s_w_edge' %(name))
+    b = layer.get_variable(shape=[c_out], \
+        trainable=True, name='%s_b' %(name))
+
+    """ -=-=-= Term 1: node aggregation =-=-=- """
+    term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
+
+    """ -=-=-= Term 2: edge aggregation =-=-=- """
+    wn = tf.linalg.matmul(node, w_node_n, name='%s_term2_wn' %(name)) # N x c_out
+    we = tf.linalg.matmul(edge, w_edge, name='%s_term2_we' %(name))  # N x num_edge x c_out
+    gather_n = tf.gather(wn, hood)
+    node_avg = tf.reduce_sum(gather_n, 1)
+    edge_avg = tf.reduce_sum(we, 1)
+    numerator = node_avg + edge_avg
+    denominator = tf.maximum(hood_in, tf.ones_like(hood_in))
+    term2 = tf.math.divide(numerator, denominator)  # (n_verts, v_filters)
+
+    y = term1 + term2 + b
+    if(verbose): print("N-E-Avg (%s)" %(name), node.shape, "->", y.shape)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def pipgcn_order_dependent(layer, node, edge, hood, c_out, \
+    batch_norm=False, activation=None, name='', verbose=True):
+
+    node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
+    edge_in_o = edge.get_shape().as_list()[-2]
+    edge_in_f = edge.get_shape().as_list()[-1]
+    hood = tf.squeeze(hood, axis=2)
+    hood_in = tf.expand_dims(tf.math.count_nonzero(hood + 1, axis=1, dtype=tf.float32), -1)
+
+    w_node_c = layer.get_variable(shape=[node_in, c_out], \
+        trainable=True, name='%s_w_node_c' %(name))
+    w_node_n = layer.get_variable(shape=[node_in, c_out], \
+        trainable=True, name='%s_w_node_n' %(name))
+    w_edge_order = layer.get_variable(shape=[edge_in_o, c_out], \
+        trainable=True, name='%s_w_edge_o' %(name))
+    w_edge_feat = layer.get_variable(shape=[edge_in_f, c_out], \
+        trainable=True, name='%s_w_edge_f' %(name))
+    b = layer.get_variable(shape=[c_out], \
+        trainable=True, name='%s_b' %(name))
+
+    """ -=-=-= Term 1: node aggregation =-=-=- """
+    term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
+
+    """ -=-=-= Term 2: edge aggregation =-=-=- """
+    wn = tf.linalg.matmul(node, w_node_n, name='%s_term2_wn' %(name)) # N x c_out
+    we_o = tf.linalg.matmul(tf.transpose(edge, perm=[0, 2, 1]), w_edge_order, name='%s_term2_we_o' %(name))  # N x num_edge x c_out
+    we_f = tf.linalg.matmul(edge, w_edge_feat, name='%s_term2_we_f' %(name))  # N x num_edge x c_out
+    gather_n = tf.gather(wn, hood)
+    node_avg = tf.reduce_sum(gather_n, 1)
+    edge_order = tf.reduce_sum(we_o, 1) + tf.reduce_sum(we_f, 1)
+    numerator = node_avg + edge_order
+    denominator = tf.maximum(hood_in, tf.ones_like(hood_in))
+    term2 = tf.math.divide(numerator, denominator)  # (n_verts, v_filters)
+
+    y = term1 + term2 + b
+    if(verbose): print("Order-N-E-Avg (%s)" %(name), node.shape, "->", y.shape)
+    return layer.activation(x=y, activation=activation, name=name)
+
+def merge_ligand_receptor(layer, ligand, receptor, pair, verbose=True):
+
+    side_lgnd = tf.gather(ligand, pair[:, 0]) # select ligand node via pair minibatch
+    side_rcpt = tf.gather(receptor, pair[:, 1]) # select receptor node via pair minibatch
+
+    y = tf.concat([side_lgnd, side_rcpt], axis=1)
+    if(verbose): print("Merge", ligand.shape, receptor.shape, "->", y.shape)
+    return y
--- whiteboxlayer/layers.py
+++ whiteboxlayer/layers.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tensorflow as tf
 
 class Layers(object):
@@ -11,7 +10,7 @@ class Layers(object):
 
     """ -*-*-*-*-*- Variables -*-*-*-*-*- """
 
-    def __get_variable(self, shape, constant=None, trainable=True, name=''):
+    def get_variable(self, shape, constant=None, trainable=True, name=''):
 
         try: return self.parameters[name]
         except:
@@ -99,9 +98,9 @@ class Layers(object):
         filter_size=[3, 16, 32], dilations=[1, 1, 1], \
         padding='SAME', batch_norm=False, activation=None, name='', verbose=True):
 
-        w = self.__get_variable(shape=filter_size, \
+        w = self.get_variable(shape=filter_size, \
             trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[filter_size[-1]], \
+        b = self.get_variable(shape=[filter_size[-1]], \
             trainable=True, name='%s_b' %(name))
 
         wx = tf.nn.conv1d(
@@ -125,13 +124,9 @@ class Layers(object):
         filter_size=[3, 16, 32], dilations=[1, 1, 1], \
         padding='SAME', batch_norm=False, activation=None, name='', verbose=True):
 
-        # for idx_os, _ in enumerate(output_shape):
-        #     if(idx_os == 0): continue
-        #     output_shape[idx_os] = int(output_shape[idx_os])
-
-        w = self.__get_variable(shape=filter_size, \
+        w = self.get_variable(shape=filter_size, \
             trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[filter_size[-2]], \
+        b = self.get_variable(shape=[filter_size[-2]], \
             trainable=True, name='%s_b' %(name))
 
         wx = tf.nn.conv1d_transpose(
@@ -156,9 +151,9 @@ class Layers(object):
         filter_size=[3, 3, 16, 32], dilations=[1, 1, 1, 1], \
         padding='SAME', batch_norm=False, activation=None, name='', verbose=True):
 
-        w = self.__get_variable(shape=filter_size, \
+        w = self.get_variable(shape=filter_size, \
             trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[filter_size[-1]], \
+        b = self.get_variable(shape=[filter_size[-1]], \
             trainable=True, name='%s_b' %(name))
 
         wx = tf.nn.conv2d(
@@ -182,9 +177,9 @@ class Layers(object):
         filter_size=[3, 3, 16, 32], dilations=[1, 1, 1, 1], \
         padding='SAME', batch_norm=False, activation=None, name='', verbose=True):
 
-        w = self.__get_variable(shape=filter_size, \
+        w = self.get_variable(shape=filter_size, \
             trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[filter_size[-2]], \
+        b = self.get_variable(shape=[filter_size[-2]], \
             trainable=True, name='%s_b' %(name))
 
         wx = tf.nn.conv2d_transpose(
@@ -210,9 +205,9 @@ class Layers(object):
 
         c_in, c_out = x.get_shape().as_list()[-1], int(c_out)
 
-        w = self.__get_variable(shape=[c_in, c_out], \
+        w = self.get_variable(shape=[c_in, c_out], \
             trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[c_out], \
+        b = self.get_variable(shape=[c_out], \
             trainable=True, name='%s_b' %(name))
 
         wx = tf.linalg.matmul(x, w, name='%s_mul' %(name))
@@ -222,165 +217,3 @@ class Layers(object):
         if(batch_norm): y = self.batch_normalization(x=y, \
             trainable=True, name='%s_bn' %(name), verbose=verbose)
         return self.activation(x=y, activation=activation, name=name)
-
-    """ -*-*-*-*-*- Custom Functions -*-*-*-*-*- """
-    def trim_odd(self, x):
-
-        b, t, c = list(x.get_shape())
-        if(int(t) % 2 == 1):
-            return tf.slice(x, [0, 0, 0], [-1, int(t)-1, -1])
-        else:
-            return x
-
-    def trim_shape(self, x, shape):
-
-        return tf.slice(x, [0, 0, 0], shape)
-
-    def sub_pixel1d(self, x, ratio, verbose=True):
-
-        y1 = tf.transpose(a=x, perm=[2, 1, 0]) # (r, w, b)
-        y2 = tf.batch_to_space(y1, [ratio], [[0, 0]]) # (1, r*w, b)
-        y3 = tf.transpose(a=y2, perm=[2, 1, 0])
-
-        if(verbose): print("SubPixel", x.shape, "->", y3.shape)
-        return y3
-
-    def graph_conv(self, x, a, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        c_in, c_out = x.get_shape().as_list()[-1], int(c_out)
-
-        w = self.__get_variable(shape=[c_in, c_out], \
-            trainable=True, name='%s_w' %(name))
-        b = self.__get_variable(shape=[c_out], \
-            trainable=True, name='%s_b' %(name))
-
-        wx = tf.linalg.matmul(x, w, name='%s_mul' %(name))
-        y_feat = tf.math.add(wx, b, name='%s_add' %(name))
-        y = tf.linalg.matmul(a, y_feat)
-
-        if(verbose): print("G-Conv (%s)" %(name), x.shape, "->", y.shape)
-
-        if(batch_norm): y = self.batch_normalization(x=y, \
-            trainable=True, name='%s_bn' %(name), verbose=verbose)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def graph_attention(self, x, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        wx = self.fully_connected(x=x, c_out=c_out, \
-            batch_norm=False, activation=None, name=name, verbose=False)
-
-        y = tf.math.reduce_sum(wx, axis=-1)
-
-        if(verbose): print("Readout (%s)" %(name), x.shape, "->", y.shape)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def read_out(self, x, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        wx = self.fully_connected(x=x, c_out=c_out, \
-            batch_norm=False, activation=None, name=name, verbose=False)
-
-        y = tf.math.reduce_sum(wx, axis=-1)
-
-        if(verbose): print("Readout (%s)" %(name), x.shape, "->", y.shape)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def pipgcn_node_average(self, node, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
-
-        w_node_c = self.__get_variable(shape=[node_in, c_out], \
-            trainable=True, name='%s_w_node_c' %(name))
-        b = self.__get_variable(shape=[c_out], \
-            trainable=True, name='%s_b' %(name))
-
-        """ -=-=-= Term 1: node aggregation =-=-=- """
-        term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
-
-        y = term1 + b
-        if(verbose): print("N-Avg (%s)" %(name), node.shape, "->", y.shape)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def pipgcn_node_edge_average(self, node, edge, hood, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
-        edge_in = edge.get_shape().as_list()[-1]
-        hood = tf.squeeze(hood, axis=2)
-        hood_in = tf.expand_dims(tf.math.count_nonzero(hood + 1, axis=1, dtype=tf.float32), -1)
-
-        w_node_c = self.__get_variable(shape=[node_in, c_out], \
-            trainable=True, name='%s_w_node_c' %(name))
-        w_node_n = self.__get_variable(shape=[node_in, c_out], \
-            trainable=True, name='%s_w_node_n' %(name))
-        w_edge = self.__get_variable(shape=[edge_in, c_out], \
-            trainable=True, name='%s_w_edge' %(name))
-        b = self.__get_variable(shape=[c_out], \
-            trainable=True, name='%s_b' %(name))
-
-        """ -=-=-= Term 1: node aggregation =-=-=- """
-        term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
-
-        """ -=-=-= Term 2: edge aggregation =-=-=- """
-        wn = tf.linalg.matmul(node, w_node_n, name='%s_term2_wn' %(name)) # N x c_out
-        we = tf.linalg.matmul(edge, w_edge, name='%s_term2_we' %(name))  # N x num_edge x c_out
-        gather_n = tf.gather(wn, hood)
-        node_avg = tf.reduce_sum(gather_n, 1)
-        edge_avg = tf.reduce_sum(we, 1)
-        numerator = node_avg + edge_avg
-        denominator = tf.maximum(hood_in, tf.ones_like(hood_in))
-        term2 = tf.math.divide(numerator, denominator)  # (n_verts, v_filters)
-
-        y = term1 + term2 + b
-        if(verbose): print("N-E-Avg (%s)" %(name), node.shape, "->", y.shape)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def pipgcn_order_dependent(self, node, edge, hood, c_out, \
-        batch_norm=False, activation=None, name='', verbose=True):
-
-        node_in, c_out = node.get_shape().as_list()[-1], int(c_out)
-        edge_in_o = edge.get_shape().as_list()[-2]
-        edge_in_f = edge.get_shape().as_list()[-1]
-        hood = tf.squeeze(hood, axis=2)
-        hood_in = tf.expand_dims(tf.math.count_nonzero(hood + 1, axis=1, dtype=tf.float32), -1)
-
-        w_node_c = self.__get_variable(shape=[node_in, c_out], \
-            trainable=True, name='%s_w_node_c' %(name))
-        w_node_n = self.__get_variable(shape=[node_in, c_out], \
-            trainable=True, name='%s_w_node_n' %(name))
-        w_edge_order = self.__get_variable(shape=[edge_in_o, c_out], \
-            trainable=True, name='%s_w_edge_o' %(name))
-        w_edge_feat = self.__get_variable(shape=[edge_in_f, c_out], \
-            trainable=True, name='%s_w_edge_f' %(name))
-        b = self.__get_variable(shape=[c_out], \
-            trainable=True, name='%s_b' %(name))
-
-        """ -=-=-= Term 1: node aggregation =-=-=- """
-        term1 = tf.linalg.matmul(node, w_node_c, name='%s_term1' %(name)) # N x c_out
-
-        """ -=-=-= Term 2: edge aggregation =-=-=- """
-        wn = tf.linalg.matmul(node, w_node_n, name='%s_term2_wn' %(name)) # N x c_out
-        we_o = tf.linalg.matmul(tf.transpose(edge, perm=[0, 2, 1]), w_edge_order, name='%s_term2_we_o' %(name))  # N x num_edge x c_out
-        we_f = tf.linalg.matmul(edge, w_edge_feat, name='%s_term2_we_f' %(name))  # N x num_edge x c_out
-        gather_n = tf.gather(wn, hood)
-        node_avg = tf.reduce_sum(gather_n, 1)
-        edge_order = tf.reduce_sum(we_o, 1) + tf.reduce_sum(we_f, 1)
-        numerator = node_avg + edge_order
-        denominator = tf.maximum(hood_in, tf.ones_like(hood_in))
-        term2 = tf.math.divide(numerator, denominator)  # (n_verts, v_filters)
-
-        y = term1 + term2 + b
-        if(verbose): print("Order-N-E-Avg (%s)" %(name), node.shape, "->", y.shape)
-        return self.activation(x=y, activation=activation, name=name)
-
-    def merge_ligand_receptor(self, ligand, receptor, pair, verbose=True):
-
-        side_lgnd = tf.gather(ligand, pair[:, 0]) # select ligand node via pair minibatch
-        side_rcpt = tf.gather(receptor, pair[:, 1]) # select receptor node via pair minibatch
-
-        y = tf.concat([side_lgnd, side_rcpt], axis=1)
-        if(verbose): print("Merge", ligand.shape, receptor.shape, "->", y.shape)
-        return y
