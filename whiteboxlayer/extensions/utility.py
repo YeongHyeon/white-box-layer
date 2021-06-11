0d2c5fd0f3067e67a9ed6ad1c2548037586901cb
박영현(Yeonghyeon Park)/IoT솔루션사업그룹/SKP <young200405@gmail.com>
2021-06-11 14:25:05 +0900

    update_20210611

--- whiteboxlayer/extensions/utility.py
+++ whiteboxlayer/extensions/utility.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def trim_odd(layer, x):
+def trim_odd(x):
 
     b, t, c = list(x.get_shape())
     if(int(t) % 2 == 1):
@@ -8,11 +8,11 @@ def trim_odd(layer, x):
     else:
         return x
 
-def trim_shape(layer, x, shape):
+def trim_shape(x, shape):
 
     return tf.slice(x, [0, 0, 0], shape)
 
-def sub_pixel1d(layer, x, ratio, verbose=True):
+def sub_pixel1d(x, ratio, verbose=True):
 
     y1 = tf.transpose(a=x, perm=[2, 1, 0]) # (r, w, b)
     y2 = tf.batch_to_space(y1, [ratio], [[0, 0]]) # (1, r*w, b)
@@ -20,3 +20,12 @@ def sub_pixel1d(layer, x, ratio, verbose=True):
 
     if(verbose): print("SubPixel", x.shape, "->", y3.shape)
     return y3
+
+def attention(x):
+
+    list_shape = x.get_shape().as_list()
+
+    xr = tf.reshape(x, [list_shape[0], -1])
+    y = tf.reshape(tf.nn.softmax(xr, axis=1), list_shape)
+
+    return y
