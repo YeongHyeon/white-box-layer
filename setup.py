0046521998bc0502eb3b33757b17913e40cd96c5
박영현(Yeonghyeon Park)/IoT솔루션사업그룹/SKP <young200405@gmail.com>
2021-12-18 12:17:54 +0900

    0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name                = 'whiteboxlayer',
-    version             = '0.1.18',
+    version             = '0.2.0',
     description         = 'TensorFlow based custom layers',
     author              = 'YeongHyeon Park',
     author_email        = 'young200405@gmail.com',
@@ -14,7 +14,7 @@ setup(
     download_url        = 'https://github.com/YeongHyeon/white-box-layer/archive/refs/heads/master.zip',
     long_description    = README,
     long_description_content_type   = "text/markdown",
-    install_requires    = ['tensorflow==2.3.0'],
+    install_requires    = ['tensorflow==2.4.0'],
     packages            = find_packages(exclude = []),
     keywords            = ['whiteboxlayer'],
     python_requires     = '>=3',
