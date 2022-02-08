545fe78165a940fdfee8767044bba1f1c2c03119
박영현(Yeonghyeon Park)/Product개발팀/SKP <young200405@gmail.com>
2022-02-08 12:43:23 +0900

    losses

--- whiteboxlayer/extensions/losses.py
+++ whiteboxlayer/extensions/losses.py
@@ -1,25 +1,39 @@
 import tensorflow as tf
 
-def loss_l1(x, reduce=None):
+def loss_ae(x, reduce=None):
 
-    distance = tf.math.reduce_mean(\
+    loss = tf.math.reduce_mean(\
         tf.math.abs(x), axis=reduce)
 
-    return distance
+    return loss
 
-def loss_l2(x, reduce=None):
+def loss_mse(x, reduce=None):
 
-    distance = tf.math.reduce_mean(\
+    loss = tf.math.reduce_mean(\
+        tf.math.square(x), axis=reduce)
+
+    return loss
+
+def loss_rmse(x, reduce=None):
+
+    loss = tf.math.reduce_mean(\
         tf.math.sqrt(\
             tf.math.square(x) + 1e-30), axis=reduce)
 
-    return distance
+    return loss
 
-def loss_l2_log(x, reduce=None):
+def loss_log_mse(x, reduce=None):
 
-    distance = tf.math.reduce_mean(\
+    loss = tf.math.reduce_mean(\
         -tf.math.log(\
-            1-tf.math.sqrt(\
-                tf.math.square(x) + 1e-30) + 1e-30), axis=reduce)
+            1 - tf.math.square(x) + 1e-30), axis=reduce)
 
-    return distance
+    return loss
+
+def loss_bce(true, pred, reduce=None):
+
+    term1 = true * tf.math.log(pred + 1e-30)
+    term2 = (1 - true) * tf.math.log(1 - pred + 1e-30)
+    loss = tf.math.reduce_mean(-(term1 + term2), axis=reduce)
+
+    return loss
